612e5d78c0b1245d80cb6fb48bf900d6e9d3d351
sanuv9683 <sanuv9683@gmail.com>
2023-05-17 11:54:28 +0530

    Finalized the example

--- src/lk/ijse/demo/Main.java
+++ src/lk/ijse/demo/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
 //        Pasindu pasindu = new Pasindu(new Saduni());
         DI di = new Pasindu();
-        di.setInjection(new Dasuni());
-        ((Pasindu)di).pasinduIsChatting();
-        ((Pasindu)di).danceWithSaduni();
+        //DI means super reference. but DI interface does not include the below methods expect inject
+        //they are inside the Sub Class Pasindu
+        // So here we can use class casting to resolve this matter
+        // Here we can cast DI(Super Interface) to its Sub Class as below
+        di.setInjection(new Dasuni()); // here no need to cast. because DI reference owns inject method
+        ((Pasindu)di).pasinduIsChatting();// casting DI to its Sub Class Pasindu
+        ((Pasindu)di).danceWithSaduni();// casting DI to its Sub Class Pasindu
 
     }
 }
