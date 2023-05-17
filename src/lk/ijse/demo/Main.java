395b43ed351e85125664b3dad36dd722e982791b
sanuv9683 <sanuv9683@gmail.com>
2023-05-17 11:51:27 +0530

    Content Updated

--- src/lk/ijse/demo/Main.java
+++ src/lk/ijse/demo/Main.java
@@ -3,10 +3,10 @@ package lk.ijse.demo;
 public class Main {
     public static void main(String[] args) {
 //        Pasindu pasindu = new Pasindu(new Saduni());
-        DI pasindu = new Pasindu();
-        ((Pasindu)pasindu).setInjection(new Dasuni());
-        ((Pasindu)pasindu).pasinduIsChatting();
-        ((Pasindu)pasindu).danceWithSaduni();
+        DI di = new Pasindu();
+        di.setInjection(new Dasuni());
+        ((Pasindu)di).pasinduIsChatting();
+        ((Pasindu)di).danceWithSaduni();
 
     }
 }
